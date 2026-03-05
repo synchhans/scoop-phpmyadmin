a1aa92c143790d705f7cd186629bcefd7622343e
Muhamad Farhan <muhamadfarhan.inc@gmail.com>
2026-03-05 23:23:19 +0700

    v2

--- bucket/phpmyadmin.ps1
+++ bucket/phpmyadmin.ps1
@@ -1,6 +1,58 @@
+param(
+    [string]$action = "start"
+)
+
 $dir = "$env:USERPROFILE\scoop\apps\phpmyadmin\current"
+$port = 8080
+$pidFile = "$dir\phpmyadmin.pid"
 
-Write-Host "Starting phpMyAdmin server..."
-Start-Process "http://localhost:8080"
+function Start-PhpMyAdmin {
+    if (Test-Path $pidFile) {
+        Write-Host "phpMyAdmin already running."
+        return
+    }
 
-php -S localhost:8080 -t $dir
+    Write-Host "Starting phpMyAdmin on http://localhost:$port"
+
+    $proc = Start-Process php `
+        "-S localhost:$port -t `"$dir`"" `
+        -PassThru -WindowStyle Hidden
+
+    $proc.Id | Out-File $pidFile
+
+    Start-Process "http://localhost:$port"
+}
+
+function Stop-PhpMyAdmin {
+    if (!(Test-Path $pidFile)) {
+        Write-Host "phpMyAdmin is not running."
+        return
+    }
+
+    $pid = Get-Content $pidFile
+    Stop-Process -Id $pid -Force
+
+    Remove-Item $pidFile
+
+    Write-Host "phpMyAdmin stopped."
+}
+
+function Status-PhpMyAdmin {
+    if (Test-Path $pidFile) {
+        Write-Host "phpMyAdmin is running."
+    } else {
+        Write-Host "phpMyAdmin is not running."
+    }
+}
+
+switch ($action) {
+    "start" { Start-PhpMyAdmin }
+    "stop" { Stop-PhpMyAdmin }
+    "status" { Status-PhpMyAdmin }
+    default {
+        Write-Host "Usage:"
+        Write-Host "  phpmyadmin start"
+        Write-Host "  phpmyadmin stop"
+        Write-Host "  phpmyadmin status"
+    }
+}
